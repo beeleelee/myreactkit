24e3bed8d8e303c312996a9d7aa89591f5a81789
lifeng <beeleelee@outlook.com>
2018-11-17 20:20:47 +0800

    kit LazyPic

---
+++ src/styles/lazy.js
@@ -0,0 +1,11 @@
+const lazyStyle = {
+  backgroundSize: '20px 20px',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  minWidth: 100,
+  minHeight: 100
+}
+
+export {
+  lazyStyle
+}
